de8951c7ffe64d940a24efac750318c7c9abcff3
rishitathota <ayushjoshi165@gmail.com>
2024-05-06 11:53:44 -0700

    removed unwanted code and added comments

--- usb_device_manger.py
+++ usb_device_manger.py
@@ -1,14 +1,21 @@
+# Importing the pywinusb.hid module for interacting with HID devices
 import pywinusb.hid as hid
-from tabulate import tabulate
+# Importing the tabulate module for formatting data into tables  
+from tabulate import tabulate  
 
 class USBDeviceManager:
+    #Initializes a USBDeviceManager object.
     def __init__(self):
-        self.devices = []
-
-    def find_usb_devices(self):
-        all_hids = hid.find_all_hid_devices()
+        # List to store USB devices information
+        self.devices = []  
+     
+    #Retrieves USB devices information and populates the devices list.
+    def get_usb_devices(self):
+        # Get all HID devices connected to the system
+        all_hids = hid.find_all_hid_devices()  
 
         for device in all_hids:
+            # Extract relevant information from the device and add it to the devices list
             self.devices.append({
                 "Manufacturer": device.vendor_name,
                 "Product": device.product_name,
@@ -16,9 +23,11 @@ class USBDeviceManager:
                 "Vendor ID": device.vendor_id,
                 "Product ID": device.product_id
             })
+    #Prints the USB devices information in a tabular format.
+    def print_in_tabular_format(self):
+        table_data = []  # List to store data for tabulate
 
-    def print_usb_devices_table(self):
-        table_data = []
+        # Prepare data for tabulate
         for device in self.devices:
             table_data.append([
                 device["Manufacturer"],
@@ -27,9 +36,14 @@ class USBDeviceManager:
                 device["Vendor ID"],
                 device["Product ID"]
             ])
+
+        # Print tabulated data
         print(tabulate(table_data, headers=["Manufacturer", "Product", "Serial Number", "Vendor ID", "Product ID"], tablefmt="grid"))
 
 if __name__ == "__main__":
+    # Create an instance of USBDeviceManager
     usb_manager = USBDeviceManager()
-    usb_manager.find_usb_devices()
-    usb_manager.print_usb_devices_table()
+    # Retrieve USB devices information
+    usb_manager.get_usb_devices()
+    # Print USB devices information in tabular format
+    usb_manager.print_in_tabular_format()
